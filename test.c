f385f57840a6a95a058a787ec65ef8c62ded2813
Philipp Kaeser <kaeser@gubbe.ch>
2025-06-02 09:08:27 +0300

    More work.

---
+++ test.c
@@ -0,0 +1,19 @@
+/* ========================================================================= */
+/**
+ * @file test.c
+ * Copyright (c) 2025 by Philipp Kaeser <kaeser@gubbe.ch>
+ */
+
+/**
+ * @mainpage This is a test.
+ * Bla Bla Bla.
+ */
+
+/**
+ * @return 0 on success.
+ */
+int main(void)
+{
+    return 0;
+}
+/* == End of test.c ================================================== */
